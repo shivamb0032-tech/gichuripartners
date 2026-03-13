30f7c5507276eb56282e2df63c18dc447e36daae
= <vivekpayal.dev@gmail.com>
2026-03-13 19:10:31 +0530

    accounting page

--- app/accounting-services-in-kenya/page.js
+++ app/accounting-services-in-kenya/page.js
@@ -4,13 +4,7 @@ import AccountHero from '@/components/Accounting/AccountHero'
 import EmpoweringSec from '@/components/Services/EmpoweringSec'
 import AccountService from '@/components/Accounting/AccountService'
 import BestAccounting from '@/components/Accounting/BestAccounting'
-import BussinessServ from '@/components/Services/BussinessServ'
 import BussnessServe from '@/components/Accounting/BussnessServe'
-import TypeAudit from '@/components/Services/TypeAudit'
-import WhyChoose from '@/components/Services/WhyChoose'
-import SecureService from '@/components/Services/SecureService'
-import ValueAddedServices from '@/components/Services/ValueAddedServices'
-import ChooseUs from '@/components/Services/ChooseUs'
 import ServiceForm from '@/components/Services/ServiceForm'
 export default function Accounting() {
  
@@ -22,7 +16,6 @@ export default function Accounting() {
     <AccountService/>
     <BestAccounting/>
     <BussnessServe/>
-   
     <ChooseUs/>
     <ServiceForm/>
     </>
