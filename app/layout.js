caa895548d6264cece4dfbaee50aa6f9b2b4cfae
= <vivekpayal.dev@gmail.com>
2026-03-20 10:23:35 +0530

    layout changes for admin and webiste

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
 import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
 import { config } from "@fortawesome/fontawesome-svg-core";  
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
@@ -14,10 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
+       
         <main>{children}</main>
-        <Footer />
+       
       </body>
     </html>
   )
 }
+
+
